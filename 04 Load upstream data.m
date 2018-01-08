f404c5c80a871a12ee1a441b884b90e7cc253e4b
MacarioBW <35231785+MacarioBW@users.noreply.github.com>
2018-01-08 15:40:51 -0500

    Update 04 Load upstream data.m
    Added a region for where chemicals are measured.

--- 04 Load upstream data.m
+++ 04 Load upstream data.m
@@ -1,5 +1,5 @@
 BW Header
 
-Load all data relating to chemicals measured upstream from the fish farm and prune as needed.
+Load all data relating to chemicals measured upstream and downstream from the fish farm and prune as needed.
 
 * EOF *
