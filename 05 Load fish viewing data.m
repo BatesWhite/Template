2467749dca76cc5ae4c3945f48bdc7a4174893f0
MacarioBW <35231785+MacarioBW@users.noreply.github.com>
2018-01-08 15:29:13 -0500

    Create 05 Load fish viewing data.m
    Here we load datasets that track the number of tourists that visit the area downstream from the fish farm.

---
+++ 05 Load fish viewing data.m
@@ -0,0 +1,5 @@
+BW Header
+
+Load data on tourist visits downstream from the fish farm including the amount of spent time visiting the downstream area and the amount of live fish available for viewing
+
+* EOF *
