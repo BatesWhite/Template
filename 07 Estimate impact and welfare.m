10580137d4461500be134bdcdf21112df6d17be6
MacarioBW <35231785+MacarioBW@users.noreply.github.com>
2018-01-08 15:42:12 -0500

    Update 07 Estimate impact and welfare.m
    Cosmetic changes.

--- 07 Estimate impact and welfare.m
+++ 07 Estimate impact and welfare.m
@@ -1,3 +1,7 @@
 BW Header
 
+
 Estimate the impact of the fish farm on tourist visits to the downstream area and calculate the overal welfare to the owner of the fish farm and the park owner.
+
+
+* EOF *
