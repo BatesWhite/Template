85f641f3c07e4c77937f43549948d4c833866a9a
MacarioBW <35231785+MacarioBW@users.noreply.github.com>
2018-01-08 15:37:34 -0500

    Update 01 Run programs.m
    I added a richer description and fixed a typo.

--- 01 Run programs.m
+++ 01 Run programs.m
@@ -1,7 +1,11 @@
 BW Header
 
 
-Input modeling sets and options (macros/globals
+** these scripts estimate the impact that an upstream fish farm has on downstream visits to a tourist destination where people spend their time viewing fish.
+
+Input modeling sets and options (macros/globals)
 
 
 Run M-files
+
+* EOF *
